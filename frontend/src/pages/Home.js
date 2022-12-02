c5b90433e5fab634624c7da3d1e32d65fe47ce87
Ezart <ezart@gmail.com>
2022-12-03 01:02:59 +0300

    inital commit

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,14 @@
+import { Container } from "react-bootstrap";
+const Home = () => {
+  return (
+    <Container
+      fluid
+      style={{ backgroundColor: "#9FA6B2", height: "100vh" }}
+      className="h-100"
+    >
+      <header>Memory API Demo (React frontend & C# backend)</header>
+    </Container>
+  );
+};
+
+export default Home;
