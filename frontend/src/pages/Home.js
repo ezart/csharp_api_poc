86c4beca431f7a7a6713d80c328f2bd5bcc4ad40
ezart <ezart193@gmail.com>
2022-12-16 02:24:41 +0300

    wip

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -7,14 +7,13 @@ import { Container, Row, Form, Button, Col, Spinner } from "react-bootstrap";
 //   );
 // };
 const Home = () => {
-  const [result, setResult] = useState(false);
+  const [result, setResult] = useState(null);
   const [num1, setNum1] = useState();
   const [num2, setNum2] = useState();
   const [history, setHistory] = useState([]);
   const [isLoading, setLoading] = useState(false)
 
   async function getHistory() {
-    console.log("hi");
     const response = await fetch("http://localhost:5000/Calculator");
     const h = await response.json();
     setHistory(h);
@@ -22,27 +21,38 @@ const Home = () => {
   }
 
   async function calculate(){
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    const payload =JSON.stringify({
+      "num1": num1,
+      "num2": num2
+    });
+
+    let requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: payload,
+      redirect: 'follow'
+    };
+
     try {
       setLoading(true)
-      const data = {
-        "num1":2, 
-        "num2":2
-      }
-      const response = await fetch("http://localhost:5000/Calculator/",{
-        method:'POST',
-        mode:'no-cors',
-        headers: {
-          'Content-Type': 'application/json'
-          // 'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body:JSON.stringify(data)
-      })
+      setResult(null)
+      
+      const response = await fetch("http://localhost:5000/Calculator/",requestOptions);
+      if(response.status === 201 || response.status === 200){
+        const res = await response.json();
+        console.log(res.result)
+        setResult(res.result)
+        // console.log(res);
+        setLoading(false)
 
-      const res = await response.json();
-      console.log(res)
-      // setResult(res.result)
-      // console.log(res);
-      // setLoading(false)
+      }else{
+        setLoading(false)
+        setResult("Error During Calcuation")
+      }
+
+      
         
     } catch (error) {
       console.log(error)
@@ -106,7 +116,8 @@ const Home = () => {
         </Col>
         <Col sm="6" className="mt-5">
           <h2>history</h2>
-          <button onClick={getHistory}>Get hisory</button>
+          <button className="btn btn-success mr-5" onClick={getHistory}>Get hisory</button>
+          <button className="btn btn-warning" onClick={()=>setHistory([])}>Clear History</button>
           <table className="table table-dark">
             <thead className="thead-dark"><tr>
               <th scope="col">Number 1</th>
@@ -116,7 +127,12 @@ const Home = () => {
             </tr></thead>
             
             {history.map((h, index) => (
-              <tr key={index}> <td>{h.num1}</td>  <td> {h.num2}</td>  <td>{h.result}</td> <td>{h.time}</td>}</tr>
+              <tr key={index}>
+                <td>{h.num1}</td>  
+                <td> {h.num2}</td>  
+                <td>{h.result}</td> 
+                <td>{Date(h.time.toString())}</td>
+              </tr>
             ))}
 
           </table>
